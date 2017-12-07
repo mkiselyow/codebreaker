8af868acf654fb49f95314535469cbfccc0ff54b
MaximKiselyow <m.kiselyow@gmail.com>
2017-12-07 18:54:04 +0200

    some progress

--- lib/codebreaker.rb
+++ lib/codebreaker.rb
@@ -3,11 +3,16 @@ require "faker"
 
 
 module Codebreaker
+  SECRET_CODE_LENGTH = 4
   class Game
-    attr_accessor :secret_code, :attempts_count
+    attr_accessor :secret_code, :attempts, :win_or_lose, :hints
     def initialize
-      @secret_code = :start
-      @attempts_count = 0
+      #load game history method
+      start
+      @attempts = []
+      @hints = []
+      @attempts_count_limit = 4
+      @win_or_lose = 'lose'
     end
 
     def start
@@ -17,34 +22,60 @@ module Codebreaker
     end
 
     def try_to_guess(attempt_to_guess)
-      @attempts_count += 1 if Attempt.new(attempt_to_guess)
-      #if you won
-      #if you lose
-      #if attemps count > 4
+      current_attempt = Attempt.new(attempt_to_guess)
+      @attempts << current_attempt
+      compare_user_input_with_secret_code(current_attempt.user_input)
+    end
+
+    private
+
+      def count_of_attempts_does_not_exceed_limit
+        @attempts_count_limit > @attempts.size
+      end
+
+      def compare_user_input_with_secret_code(current_attempt_user_input)
+        right_digits = []
+        current_attempt_user_input.map do |digit|
+          right_digits << '+' if @secret_code.include?(digit.to_i)
+        end
+        if right_digits.compact.size == SECRET_CODE_LENGTH
+          @win_or_lose = 'win'
+          raise GameEnded.new(@win_or_lose)
+        elsif count_of_attempts_does_not_exceed_limit
+          @hints << Hint.new(right_digits)
+        else
+          raise GameEnded.new(@win_or_lose)
+        end
+      end
+
+    class GameEnded < StandardError
+      def initialize(win_or_lose="The Game is ended")
+        super("YOU #{win_or_lose} THE GAME, THE GAME IS ENDED")
+        #save game history method
+      end
     end
   end
 
   class Attempt
-    attr_reader :attempt_to_guess
-    def initialize(attempt_to_guess)
-      @attempt_to_guess = attempt_to_guess.to_s if valid?(attempt_to_guess)
+    attr_reader :user_input
+    def initialize(user_input)
+      @user_input = user_input.to_s.split('') if valid?(user_input)
     end
 
-    def valid?(attempt_to_guess)
-      msg = "=== GIVE ME 4 DIGITS (1-6) ==="
-      attempt_to_guess = attempt_to_guess.to_s.split('') if !attempt_to_guess.is_a?(Array) 
-      attempt_to_guess.join.match(/([1-6]){4}/) ? true : (raise ArgumentError.new(msg))
-    end
+    private
+
+      def valid?(user_input)
+        msg = "=== GIVE ME 4 DIGITS (1-6) ==="
+        user_input = user_input.to_s.split('') if !user_input.is_a?(Array) 
+        user_input.join.match(/([1-6]){4}/) ? true : (raise ArgumentError.new(msg))
+      end
   end
 
   class Hint
     attr_reader :result
-    def initialize(attempt_to_guess_validated)
-      @result = analize(attempt_to_guess_validated)
-    end
-
-    def analize(attempt_to_guess_validated)
-      #
+    def initialize(right_digits)
+      (SECRET_CODE_LENGTH - right_digits.compact.size).times { right_digits << '-'}
+      @result = right_digits
     end
   end
 end
--- spec/codebreaker_spec.rb
+++ spec/codebreaker_spec.rb
@@ -11,11 +11,6 @@ RSpec.describe Codebreaker do
     describe Codebreaker::Game do
       let(:game) { @game = described_class.new }
 
-      it 'saves secret code' do
-        game.start
-        expect(game.instance_variable_get(:@secret_code)).not_to be_empty
-      end
-
       it 'Game.new.start generates 4 digit number' do 
         game.start
         expect(game.instance_variable_get(:@secret_code).length).to eq(4)
@@ -32,10 +27,10 @@ RSpec.describe Codebreaker do
         valid_input = []
         4.times { valid_input << Faker::Number.unique.between(1, 6)}
         game.try_to_guess(valid_input)
-        expect(game.attempts_count).to eq(1)
+        expect(game.attempts.size).to eq(1)
       end
 
-      it "raises" do
+      it "raises error if input in not valid" do
         msg = "=== GIVE ME 4 DIGITS (1-6) ==="
         invalid_input_array = [7543, 12345, 123, 'abcd']
         invalid_input_array.each do |invalid_input|
@@ -44,11 +39,27 @@ RSpec.describe Codebreaker do
         end
       end
 
-      it "gives hint if did not guess"
-      it "ends when attempts_count > 4"
-      it "gives hint if did not guess"
-      it "congradulates when win"
-      
+      it "saves secret code" do 
+        expect { game.start }.to change(game, :secret_code)
+      end
+
+      it "ends when attempts_count > 4" do 
+        game.instance_variable_set(:@secret_code, [1, 2, 3, 4])
+        expect { 4.times {game.try_to_guess(1235)} }.to raise_error(described_class::GameEnded)
+      end
+
+      it "creates hint if did not guess" do
+        game.instance_variable_set(:@secret_code, [1, 2, 3, 4])
+        expect { game.try_to_guess(1235) }.
+          to change(game, :hints) #.from([]).to([Codebreaker::Hint.new(['+','+','+'])])
+      end
+
+      it "congradulates when win" do 
+        game.instance_variable_set(:@secret_code, [1, 2, 3, 4])
+        msg = "YOU win THE GAME, THE GAME IS ENDED"
+        expect { game.try_to_guess(1234) }.to raise_error(described_class::GameEnded, msg)
+      end
+
     end
   end
 
